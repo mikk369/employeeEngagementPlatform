f66721187744b1f1491bb8a8d1f6922811574121
mikk369 <mikk.ereline@vikk.ee>
2022-11-08 13:51:41 +0200

    changed route

--- app.js
+++ app.js
@@ -9,12 +9,14 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = YAML.load('./swagger.yaml');
 
 const hourRouter = require('./routes/hoursRoutes');
-const userRouter = require('./routes/userRoutes');
+// const userRouter = require('./routes/userRoutes');
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+require('dotenv').config();
 
 //DB connection
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.PASSWORD);
@@ -24,7 +26,7 @@ mongoose.connect(DB, {
 console.log('DB connection successfull!');
 
 app.use('/hours', hourRouter);
-app.use('/users', userRouter);
+// app.use('/users', userRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
