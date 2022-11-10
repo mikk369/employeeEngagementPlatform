de9f5c6fbdc986ee7ada20bf7e010c8ddd996d86
mikk369 <mikk.ereline@vikk.ee>
2022-11-10 18:38:59 +0200

    user api

---
+++ controllers/userApi.js
@@ -0,0 +1,13 @@
+const User = require('./../models/userModel');
+
+exports.createUser = async (req, res, next) => {
+  try {
+    const newUser = await User.create(req.body);
+    res.status(201).json({
+      status: 200,
+      user: newUser,
+    });
+  } catch (err) {
+    res.status(400).json('required fields are not filled or in invalid format');
+  }
+};
