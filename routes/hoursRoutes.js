96a9e866cad711cc130f6f91a895e61465ac5f36
mikk369 <mikk.ereline@vikk.ee>
2022-11-12 11:51:25 +0200

    As user I can delete my working hours[#183674801]

--- routes/hoursRoutes.js
+++ routes/hoursRoutes.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const hoursApi = require('./../controllers/hoursApi');
+const authController = require ("./../controllers/authController")
 
-router.route('/').post(hoursApi.createHours).get(hoursApi.getAllhours);
+router.route('/').post(authController.protect,hoursApi.createHours).get(hoursApi.getAllhours);
 
 
 
 router
   .route('/:id')
-  .get(hoursApi.getHour)
-  .patch(hoursApi.updateHours)
-  .delete(hoursApi.deleteHour);
+  .get(authController.protect,hoursApi.getHour)
+  .patch(authController.protect,hoursApi.updateHours)
+  .delete(authController.protect,hoursApi.deleteHour);
 
 module.exports = router;
