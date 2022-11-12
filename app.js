685c1bd63cecec23ccb56bcbc8f2db07b5004757
mikk369 <mikk.ereline@vikk.ee>
2022-11-12 11:48:50 +0200

    cookie parser added

--- app.js
+++ app.js
@@ -7,16 +7,25 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = YAML.load('./swagger.yaml');
+const cookieParser = require("cookie-parser");
 
 const hourRouter = require('./routes/hoursRoutes');
 const userRouter = require('./routes/userRoutes');
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 require('dotenv').config();
+
+// Test middleware
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  // console.log(req.cookies);
+  next();
+});
 
 //DB connection
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.PASSWORD);
