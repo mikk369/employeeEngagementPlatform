20438799fa305befb8c6960c218ef38a488980f8
mikk369 <mikk.ereline@vikk.ee>
2022-11-24 22:21:17 +0200

    removed pwd confirm

--- models/userModel.js
+++ models/userModel.js
@@ -2,26 +2,18 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const userSchema = new mongoose.Schema({
-  name: {
+  email: {
     type: String,
-    required: [true, 'Please insert name'],
+    required: [true, 'Please provide your email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
   },
   password: {
     type: String,
     required: [true, 'please provide a password'],
     minlength: 8,
     select: false,
-  },
-  passwordConfirm: {
-    type: String,
-    required: [true, 'Please confirm your password'],
-    validate: {
-      // This only works on CREATE and SAVE!!!
-      validator: function (el) {
-        return el === this.password;
-      },
-      message: 'Passwords are not the same!',
-    },
   },
   active: {
     type: Boolean,
@@ -36,10 +28,6 @@ userSchema.pre('save', async function (next) {
 
   // Hash the password with cost of 12
   this.password = await bcrypt.hash(this.password, 12);
-
-  // Delete passwordConfirm field
-  this.passwordConfirm = undefined;
-  next();
 });
 
 userSchema.pre('save', function (next) {
