c5e320a67729f3e502e5f292189a42245a23a582
mikk369 <mikk.ereline@vikk.ee>
2022-11-24 22:24:18 +0200

    changed routes

--- routes/hoursRoutes.js
+++ routes/hoursRoutes.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const hoursApi = require('./../controllers/hoursApi');
-const authController = require ("./../controllers/authController")
+const authController = require('./../controllers/authController');
 
-router.route('/').post(authController.protect,hoursApi.createHours).get(hoursApi.getAllhours);
-
-
+router
+  .route('/')
+  .post(authController.protect, hoursApi.createHours)
+  .get(authController.protect, hoursApi.getAllhours);
 
 router
   .route('/:id')
-  .get(authController.protect,hoursApi.getHour)
-  .patch(authController.protect,hoursApi.updateHours)
-  .delete(authController.protect,hoursApi.deleteHour);
+  .get(authController.protect, hoursApi.getHour)
+  .patch(authController.protect, hoursApi.updateHours)
+  .delete(authController.protect, hoursApi.deleteHour);
 
 module.exports = router;
